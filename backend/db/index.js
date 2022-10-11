6ed273e0b8bf35c1c6e4c702e8752820bbe8200b
gabisanto <gabrielasantoriello@gmail.com>
2022-10-11 14:52:22 -0300

    more testing

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("moviehouse", null, null, {
+const db = new Sequelize("moviehouse", "ubuntu", "password123", {
   host: "localhost",
   dialect: "postgres",
   logging: false,
